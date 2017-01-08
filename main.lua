023fff016781e88a0fb13f7e4a95bb572ff4a06f
YoPox <yopoxdev+github@gmail.com>
2017-01-08 18:50:38 +0100

    Implémentation Ripto

--- main.lua
+++ main.lua
@@ -1,13 +1,49 @@
-local skwirel = RegisterMod("Skwirel")
-local acorn_item = Isaac.GetItemIdByName("Acorn")
+local Skwirel = RegisterMod("Skwirel", 1)
+local game = Game()
 
-function skwirel:use_acorn()
-  local player = Isaac.GetPlayer(0)
-	-- random number
-	local effect = math.random(0, 4)
-	if effect == 0 then -- damage
-		--body...
-	end
+local itemID = {
+    RIPTO = Isaac.GetItemIdByName("Riptogamer")
+}
+
+local hasItem = {
+    Ripto = false
+}
+
+local effects = {
+    RIPTO_DMG = 0.5,
+    RIPTO_TEARS = 0.5
+}
+
+-- Update the inventory
+local function UpdateItems(player)
+    hasItem.Ripto = player:hasCollectible(itemID.RIPTO)
 end
 
-skwirel:AddCallback(ModCallbacks.MC_USE_ITEM, skwirel.use_acorn, acorn_item);
+-- When the run starts or continues
+function Skwirel:onPlayerInit(player)
+    UpdateItems(player)
+end
+
+Skwirel:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, Skwirel.onPlayerInit)
+
+-- When passive effects should update
+function Skwirel:onUpdate(player)
+    if game:GetFrameCount() == 1 then
+        Isaac.spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, itemID.RIPTO, Vector(320, 300), Vector(0, 0), nil)
+    end
+    UpdateItems(player)
+end
+
+Skwirel:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, Skwirel.onUpdate)
+
+-- When cache is updated
+function Skwirel:onCache(player, cacheFlag)
+    if cacheFlag == cacheFlag.CACHE_DAMAGE then
+        if player:hasCollectible(itemID.RIPTO) and not hasItem.Ripto then
+            player.Damage = player.Damage + effects.RIPTO_DMG
+        end
+    end
+    UpdateItems(player)
+end
+
+Skwirel:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Skwirel.onCache);
